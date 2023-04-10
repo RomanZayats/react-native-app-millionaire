470f6f851b3218ba3955d18b12c007805c3ce4aa
Roman Zayats <zayatr01@kits.kfplc.com>
2023-04-10 15:15:12 +0300

    trivial updates

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,16 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
     plugins: [
       [
-        "module-resolver",
+        'module-resolver',
         {
-          root: ["./src"],
-          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            tests: ["./tests/"],
-            "@components": "./src/components",
+            tests: ['./tests/'],
+            '@components': './src/components',
           },
         },
       ],
